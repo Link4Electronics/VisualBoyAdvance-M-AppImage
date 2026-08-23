c3782534c690f70e26ff4f64c40d7e5b1ae6c59c
Link4Electronics <link4electronics@gmail.com>
2026-08-23 19:57:19 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor
+#pacman -Syu --noconfirm
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini libdecor-mini
 
 # Comment this out if you need an AUR package
 PRE_BUILD_CMDS='sed -i "s/-Wno-dev)/-Wno-dev\\n              -DCMAKE_CXX_FLAGS=\\"-Wno-error=attributes\\")/" ./PKGBUILD' make-aur-package vbam-git
